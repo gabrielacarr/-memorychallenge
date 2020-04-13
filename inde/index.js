95273425aa1283697008b0a471571109d5d767e4
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-13 17:55:40 -0400

    star fell random

--- inde/index.js
+++ inde/index.js
@@ -7,47 +7,67 @@ canvas.width = innerWidth //800
 canvas.height = innerHeight //600
 
 
-// FALLEN STARS
+// ASTEROID STARS
 // // CREATION
 
-function Star (x,y,radius, color) {
+function Star (x,y,radius, color) { // creating speed of the asteroid
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
+    this.velocity = {
+        x: 0,
+        y: 3
+    }
 }
 
-Star.prototype.draw = function() {
+Star.prototype.draw = function() { // creating how the asteroid look
     c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false) // center portion of circle
     c.fillStyle = this.color
     c.fill()
     c.closePath()
 }
 
-Star.prototype.update = function() {
-    this.draw()
-}
-// // IMPLEMENTATION
+function update() {  // asteroid speed function
+    
 
-let stars 
+    // if (this.y + this.radius > canvas.height){
+    // }
+
+//     this.y += this.velocity.y // asetroid speed (velocity amount)
+}
+// // APPLICATION LOOP
+
+let stars // loop for the how many attack and color // asetroid shape will be determined after logic completed 
 function init() {
     stars = []
     for (let i = 0; i < 1; i++) {
-        stars.push(new Star(canvas.wdith/ 2, 30, 30, 'blue'))
+        // stars.push(new Star(canvas.width/ 2, 30, 30, 'blue'))
+        stars.push({
+            x: Math.floor(Math.random() * (canvas.width - 30)), 
+            y: 30, 
+            width: 30, 
+            height: 30,
+            color: 'blue'})
     }
 }
 
 // // ANIMATION LOOP
 
-function animate() {
+function animate() { // asetroid will fall but need to make sure it bounces back up and not continue through page
     requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height) // clearrs the asetroid 
     stars.forEach(star => {
-        star.update();
-    });
+       // update()
+       
+       c.fillRect(star.x, star.y++, star.width, star.height)
+    })
 
 }
+
+init()
+animate()
 
 
 
