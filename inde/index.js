65dd0b9eed000b0de055b6c915ac179b26a40988
Gabriela <24256452+gabrielacarr@users.noreply.github.com>
2020-04-13 17:16:27 -0400

    stars

--- inde/index.js
+++ inde/index.js
@@ -1,70 +1,62 @@
 // CANVAS
 const canvas = document.querySelector("canvas")
-const ctx = canvas.getContext("2d")
+const c = canvas.getContext("2d")
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
 
-// let backgroundImg = new Image()
-// backgroundImg.src="/scM15f.jpg"
+canvas.width = innerWidth //800
+canvas.height = innerHeight //600
 
-let background = {
-    x:0,
-    y:0
+
+// FALLEN STARS
+// // CREATION
+
+function Star (x,y,radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
 }
 
-
-// CANVAS BACKGROUND 
-
-function drawBackground(){
-
-    ctx.drawImage(backgroundImg, background.x, background.y)
-
+Star.prototype.draw = function() {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
 }
 
-// SQUARE ANIMATION
-
-let squareX = canvas.width/2;  //placed outside because its creating the variable squareX as a axis
-
-function animate(){ // requestAnimationFrame is a loop that callback a function to go over and over again
-    window.requestAnimationFrame(animate)
-    // drawBackground()
-    ctx.clearRect(0,0,canvas.width,canvas.height)  // why i opace, what its doing  // earase what i did previous loop
-    ctx.fillRect(squareX++,canvas.height-20,20,20) // fills retangle with x,y, width, and height 
-    console.log(squareX)
-
+Star.prototype.update = function() {
+    this.draw()
 }
-animate()
+// // IMPLEMENTATION
 
-
-// KEY EVENT LISTENER 
-//let only exist within scope of the page and wont leave outside function
-
-var leftButton = false;
-var rightButton = false;
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-// KEY DOWN
-
-function keyDownHandler(event) {
-    if(event.key == "Left") {
-        leftButton = true;
-    }
-    if(event.key == "Right"){
-        rightButton = true;
+let stars 
+function init() {
+    stars = []
+    for (let i = 0; i < 1; i++) {
+        stars.push(new Star(canvas.wdith/ 2, 30, 30, 'blue'))
     }
 }
 
+// // ANIMATION LOOP
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    stars.forEach(star => {
+        star.update();
+    });
+
+}
 
 
-// KEY UP
 
-// next step, animate with buttons , method onkeyydown, onkeypress
 
-//zStep 1  - request animation frame 
 
-//Step 2 - controls keypress up down left right 
 
-//Step 3 move the background when pressing left or right 
-//make a square, make it move left and right 
+// TO DO:
+// next -  animate with buttons , method onkeyydown, onkeypress
+// next - request animation frame 
+// next - controls keypress up down left right 
+// next - move the background when pressing left or right 
+// make a square, make it move left and right 
